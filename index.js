14939d1f2cb451de5dc03af3fd77443288abb58e
Dave Strus <dave@getfretless.com>
2018-05-15 14:43:56 -0400

    Clear the input after each submit

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const handleSubmit = function(ev) {
   const f = ev.target
   const userName = f.userName.value
   users.textContent += ' ' + userName
+
+  f.userName.value = ''
 }
 
 form.addEventListener('submit', handleSubmit)
