9e4db887132297435e37127a0f707b47b9fbf17d
Dave Strus <dave@getfretless.com>
2018-05-15 15:11:10 -0400

    Reset the form after each submit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const handleSubmit = function(ev) {
   const age = f.age.value
   users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
 
-  f.userName.value = ''
+  f.reset()
 }
 
 form.addEventListener('submit', handleSubmit)
