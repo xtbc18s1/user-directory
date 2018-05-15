ac19eae76f39b47ce4aa5f869b269cb41dc78054
Dave Strus <dave@getfretless.com>
2018-05-15 10:57:04 -0400

    Add age, and reset the form each time

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const handleSubmit = function(ev) {
   ev.preventDefault()
   const form = ev.target
   const userName = form.userName.value
+  const age = form.age.value
   const users = document.querySelector('#users')
-  users.innerHTML += '<p>' + userName + '</p>'
-  form.userName.value = ''
+  users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
+  form.reset()
 }
 
 form.addEventListener('submit', handleSubmit)
