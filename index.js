97fded7401d8836297970a567644d88105cc24b7
Dave Strus <dave@getfretless.com>
2018-05-15 10:01:27 -0400

    Get the input value via the event target

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const form = document.querySelector('form#userForm')
 
 const handleSubmit = function(ev) {
   ev.preventDefault()
+  const form = ev.target
+  const userName = form.userName.value
   const heading = document.querySelector('h1')
-  const input = document.querySelector('input')
-  heading.textContent = input.value
+  heading.textContent = userName
 }
 
 form.addEventListener('submit', handleSubmit)
