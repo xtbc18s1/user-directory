92ed9814f86b5eaa1b5101be094e1733f401533f
Dave Strus <dave@getfretless.com>
2018-05-16 14:03:39 -0400

    Extract renderListItem function

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function renderColor(color) {
   return colorDiv
 }
 
+function renderListItem(label, value) {
+  const item = document.createElement('li')
+  item.textContent = `${label}: ${value}`
+  return item
+}
+
 const handleSubmit = function(ev) {
   ev.preventDefault()
   const f = ev.target
@@ -15,19 +21,13 @@ const handleSubmit = function(ev) {
   const age = f.age.value
   const favoriteColor = f.favoriteColor.value
 
-  const nameItem = document.createElement('li')
-  nameItem.textContent = `Name: ${userName}`
-
-  const ageItem = document.createElement('li')
-  ageItem.textContent = `Age: ${age}`
-
   const colorItem = document.createElement('li')
   colorItem.textContent = 'Favorite Color: '
   colorItem.appendChild(renderColor(favoriteColor))
 
   const list = document.createElement('ul')
-  list.appendChild(nameItem)
-  list.appendChild(ageItem)
+  list.appendChild(renderListItem('Name', userName))
+  list.appendChild(renderListItem('Age', age))
   list.appendChild(colorItem)
 
   const users = document.querySelector('#users')
